1eb80657b0af016cd045ab9529a114ce1c5cb982
Malcolm Ramsay <malramsay64@gmail.com>
2021-04-01 17:46:35 +1030

    feat: Incorporate telemetry using tracing

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,45 @@
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize, Debug)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, HttpResponse> {
+    let request_id = Uuid::new_v4();
+    tracing::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    tracing::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        HttpResponse::BadRequest().finish()
+    })?;
+    tracing::info!("New subscriber details have been saved");
+    Ok(HttpResponse::Ok().finish())
+}

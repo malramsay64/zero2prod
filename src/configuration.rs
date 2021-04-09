2e4f60495cd8bd56ab901dca03f0211dafc4eaa5
Malcolm Ramsay <malramsay64@gmail.com>
2021-04-09 12:26:54 +0930

    fix configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,3 +1,5 @@
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::PgConnectOptions;
 use std::convert::{TryFrom, TryInto};
 
 #[derive(serde::Deserialize)]
@@ -16,24 +18,23 @@ pub struct ApplicationSettings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: String,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
 }
 
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> String {
-        format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password, self.host, self.port, self.database_name
-        )
+    pub fn without_db(&self) -> PgConnectOptions {
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password)
+            .port(self.port)
     }
 
-    pub fn connection_string_without_db(&self) -> String {
-        format!(
-            "postgres://{}:{}@{}:{}",
-            self.username, self.password, self.host, self.port
-        )
+    pub fn with_db(&self) -> PgConnectOptions {
+        self.without_db().database(&self.database_name)
     }
 }
 
@@ -71,7 +72,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
 
     let base_path = std::env::current_dir().expect("Failed to determine the current directory.");
-    let confguration_directory = base_path.join("configuration");
+    let configuration_directory = base_path.join("configuration");
 
     // Detect the runnning environment, defaulting to "local"
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
@@ -79,8 +80,16 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .try_into()
         .expect("Failed to parse APP_ENVIRONMENT.");
 
+    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
+
     // Config knows how to read many different types of files, yaml, json, etc
-    settings.merge(config::File::with_name("configuration"))?;
+    settings.merge(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    )?;
+
+    // Add in settings from environment variables, with a prefix of APP and '__' as a separator.
+    // APP_APPLICATION__PORT=5001 would set 'Settings.application.port'
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
 
     // Try to conver to our settings type
     settings.try_into()
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -10,7 +10,9 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_with(configuration.database.with_db())
         .await
         .expect("Failed to connect to Postgres.");
 
